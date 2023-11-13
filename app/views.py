cba7e2e7e5fe90d1a5e7b8b869270413983cf60b
Елизавета Шерман <lizasherpro001@gmail.com>
2023-11-14 02:23:19 +0300

    almost hw 1-2

--- app/views.py
+++ app/views.py
@@ -21,17 +21,25 @@ QUESTIONS = [
         } for i in range (20)
     ]
 
-def paginate(objects, page, per_page=7):
-    paginator = Paginator(objects, per_page=per_page)
+TAGS = ["python", "css", "Java", "IOS"]
 
-    return paginator.page(page)
+def paginate(request, objects, per_page=7):
+    paginator = Paginator(objects, per_page=per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
 
 def index(request):
-    page = request.GET.get('page', 1)
-    return render(request, 'index.html', context={'questions' : paginate(QUESTIONS, page=page), 'page' : page })
+    content = paginate(request, QUESTIONS)
+    return render(request, 'index.html', context={'questions' : content, 'tags' : TAGS})
+
+def hot(request):
+    content = paginate(request, QUESTIONS)
+    return render(request, 'index.html', context={'questions' : content, 'tags' : TAGS})
+
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'tags' : TAGS})
 
 def question(request, question_id):
     item = QUESTIONS[question_id]
@@ -47,16 +55,20 @@ def question(request, question_id):
         'I ll worship like a dog at the shrine of your lies' +
         'I ll tell you my sins and you can sharpen your knife' +
         'Offer me that deathless death' +
-        'Good God, let me give you my life {i}'
+        f'Good God, let me give you my life {i}'
         } for i in range (3)
     ]
-    return render(request, 'question.html', {'question' : item, 'answers': answers})
+    return render(request, 'question.html', {'question' : item, 'answers': answers, 'tags' : TAGS})
 
-def tag(request):
-    return render(request, 'tag.html')
+def tag(request, tag_name):
+    content = paginate(request, QUESTIONS)
+    return render(request, 'tag.html', {'questions' : content, 'tag' : tag_name, 'tags' : TAGS})
 
 def signup(request):
-    return render(request, 'signup.html')
+    return render(request, 'signup.html', {'tags' : TAGS})
 
 def ask(request):
-    return render(request, 'ask.html')
+    return render(request, 'ask.html', {'tags' : TAGS})
+
+def settings(request):
+    return render(request, 'settings.html', {'tags' : TAGS})
--- webbb/urls.py
+++ webbb/urls.py
@@ -19,11 +19,13 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='index'),
     path('question/<int:question_id>', views.question, name='question'),
-    path('ask', views.ask),
-    path('tag', views.tag),
-    path('login', views.login),
-    path('signup', views.signup),
+    path('ask', views.ask, name='ask'),
+    path('tag/<tag_name>', views.tag, name='tag'),
+    path('login', views.login,name='login'),
+    path('register', views.signup,name='register'),
+    path('settings', views.settings, name='settings'),
+    path('hot', views.hot, name='hot'),
     path('admin/', admin.site.urls),
 ]
